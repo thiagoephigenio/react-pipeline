8ce208153c42b732465687964f9c9a46a811f3ee
Suziane dos Santos Wagner <suziane@debian.cattalini-tm.pgua>
2021-05-11 13:40:26 -0300

    comecando interface

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -41,7 +41,14 @@ class Login extends Component {
       <>
         <form onSubmit={this.handleLogin}>
           {this.state.error && <p>{this.state.error}</p>}
+        <div className="cabecalho">
+        {/* <img className="logo" src="images/logo.png"> </img> */}
+        <h4 className="titulo"> Pipeline </h4>
+        </div>
+        <div className="login">
 
+        </div>
+        <div className="email">
           <input
             type="email"
             className="inpt-login"
@@ -49,7 +56,8 @@ class Login extends Component {
             placeholder="Endereço de e-mail"
             onChange={(e) => this.setState({ email: e.target.value })}
           />
-
+        </div>
+        <div className="senha">
           <input
             type="password"
             className="inpt-login"
@@ -57,11 +65,16 @@ class Login extends Component {
             placeholder="Senha"
             onChange={(e) => this.setState({ password: e.target.value })}
           />
+        </div>
 
+        <div className="div-bt">
           <button type="submit" className="btn-login">Entrar</button>
           <hr />
+          </div>
+          
         </form>
       </>
+      
     );
   }
 }
